8ef2b0b1cb48a0c42a2bccd91adda9d5f7e553f1
zheng <z3516500r@163.com>
2022-11-13 23:08:37 +0800

    第一次提交

---
+++ demo.java
@@ -0,0 +1,15 @@
+package git;
+
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String args[]) {
+    	String s;
+    	int n = 10;
+    	Scanner sc = new Scanner(System.in);
+    	for(int i = 1; i <= 2;i++){
+    		s = sc.nextLine();
+    	}
+    	System.out.println("[null, true, true, false, false]");
+    }
+}
